efa8a195c3211623144473293bd38688f254fb08
koki.kikuchi <kikuchi.k311@gmail.com>
2022-07-16 11:58:25 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', 'TasksController@index');
+//Route::get('/', 'TasksController@index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('tasks', 'TasksController');
 
-
-
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
