fcce7f155aff7ac3201e3fadb6e60a454ee22aad
koki.kikuchi <kikuchi.k311@gmail.com>
2022-07-16 12:13:36 +0000

    user registration

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -8,34 +8,21 @@ use App\Task;
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $tasks = Task::all();
         return view('tasks.index',['tasks'=>$tasks]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         $task = new Task;
         return view('tasks.create',['task'=>$task,]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <body>
         <header class="mb-4">
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
                 <a class="navbar-brand" href="/">Tasklist</a>
 
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
@@ -20,7 +19,9 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+{{--                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li> --}}
                     </ul>
                 </div>
             </nav>
